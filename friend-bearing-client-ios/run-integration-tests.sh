dab891e3f082d394973171026c85388a47efea2b
Simon Lundmark <simon.lundmark@gmail.com>
2012-10-10 21:51:04 +0100

    Continued working on the test scripts

--- friend-bearing-client-ios/run-integration-tests.sh
+++ friend-bearing-client-ios/run-integration-tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
+
+# TODO: Start a web server so the integration tests have something to integrate with
+
 xcodebuild -project friend-bearing-client-ios-lib/friend-bearing-client-ios-lib.xcodeproj -target friend-bearing-client-ios-libIntegrationTests -sdk iphonesimulator6.0 -configuration Release clean build TEST_AFTER_BUILD=yes
