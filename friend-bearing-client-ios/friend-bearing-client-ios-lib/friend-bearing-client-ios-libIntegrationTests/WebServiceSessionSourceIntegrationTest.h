56ad7a6fcbe25b7ac2a4a9682ffe7c83ec573a0d
Simon Lundmark <simon.lundmark@gmail.com>
2012-10-10 21:36:46 +0100

    Separated some tests to an integration test target

---
+++ friend-bearing-client-ios/friend-bearing-client-ios-lib/friend-bearing-client-ios-libIntegrationTests/WebServiceSessionSourceIntegrationTest.h
@@ -0,0 +1,12 @@
+//
+//  WebServiceSessionSourceIntegrationTest.h
+//  friend-bearing-client-ios-lib
+//
+//  Created by simlun on 2012-10-10.
+//  Copyright (c) 2012 Simon Lundmark. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WebServiceSessionSourceIntegrationTest : SenTestCase
+@end
--- friend-bearing-client-ios/friend-bearing-client-ios-lib/friend-bearing-client-ios-libTests/WebServiceSessionSourceTest.m
+++ friend-bearing-client-ios/friend-bearing-client-ios-lib/friend-bearing-client-ios-libTests/WebServiceSessionSourceTest.m
@@ -7,47 +7,7 @@
 //
 
 #import "WebServiceSessionSourceTest.h"
-#import "WebServiceSessionSource.h"
-#import "InMemorySessionStorage.h"
-#import "TestUtils.h"
 
 @implementation WebServiceSessionSourceTest
-
-- (void)test_itReturnsTheSession_ifFoundInStorage
-{
-    WebServiceSessionSource *ws = [WebServiceSessionSource new];
-    InMemorySessionStorage *sessionStorage = [InMemorySessionStorage new];
-    Session *session = [Session new];
-    session.userID = @"17";
-    sessionStorage.session = session;
-    ws.sessionStorage = sessionStorage;
-    
-    __block NSString *userID = nil;
-    [ws getCurrentSessionAndSucceed:^(Session *s) {
-        userID = s.userID;
-    } orFail:nil];
-    
-    STAssertEqualObjects(userID, @"17", nil);
-}
-
-- (void)test_itGetsANewSession_viaTheWebService_ifNotFoundInStorage
-{
-    WebServiceSessionSource *ws = [WebServiceSessionSource new];
-    NSOperationQueue *queue = [NSOperationQueue new];
-    ws.queue = queue;
-    InMemorySessionStorage *sessionStorage = [InMemorySessionStorage new];
-    sessionStorage.session = nil;
-    ws.sessionStorage = sessionStorage;
-    
-    __block NSString *userID = nil;
-    __block BOOL completed = NO;
-    [ws getCurrentSessionAndSucceed:^(Session *s) {
-        userID = s.userID;
-        completed = YES;
-    } orFail:nil];
-    
-    STAssertFalse([TestUtils didWaitForMoreThan:5 secondsForTruth:&completed], nil);
-    STAssertEqualObjects(userID, @"123", nil);
-}
 
 @end
