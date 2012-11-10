a4033aa781cca254b71d9958e446a2539d4cf6d5
Simon Lundmark <simon.lundmark@gmail.com>
2012-11-10 22:29:44 +0000

    Stubbed webserver may now deliver a user bearing

--- friend-bearing-client-ios/integration-test-utils/stubbed-webserver/project.clj
+++ friend-bearing-client-ios/integration-test-utils/stubbed-webserver/project.clj
@@ -1,5 +1,6 @@
 (defproject stubbed-webserver "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.2.0"]
                  [compojure "1.1.1"]]
   :plugins [[lein-ring "0.7.3"]]
   :ring {:handler stubbed-webserver.handler/app}
--- friend-bearing-client-ios/integration-test-utils/stubbed-webserver/src/stubbed_webserver/handler.clj
+++ friend-bearing-client-ios/integration-test-utils/stubbed-webserver/src/stubbed_webserver/handler.clj
@@ -1,12 +1,18 @@
 (ns stubbed-webserver.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.data.json :as json]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (POST "/session" [] {:status 201
-                       :body "{\"user-id\": \"123\", \"session-key\": \"a1b2c3d4\"}\n"})
+  (GET "/" [] 
+       "Hello World")
+  (POST "/session" [] 
+        {:status 201
+         :body (json/write-str {:user-id 123, :session-key "a1b2c3d4"})})
+  (GET "/bearing/:user-id" [user-id]
+       {:status 200
+        :body (json/write-str {:magnetic-bearing 47})})
   (route/not-found {:status 404
                     :body "Not Found"}))
 
