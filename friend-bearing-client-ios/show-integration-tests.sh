9e2cc010b993a3e6ad40852d6e9ac048c4bed191
Simon Lundmark <simon.lundmark@gmail.com>
2012-12-01 22:52:23 +0000

    Improved show-integration-tests.sh
    - Also handles some common uppercase abbrevations better, like the
      unit-tests version.

--- friend-bearing-client-ios/show-integration-tests.sh
+++ friend-bearing-client-ios/show-integration-tests.sh
@@ -3,10 +3,15 @@ set -e
 
 cd friend-bearing-client-ios-lib/friend-bearing-client-ios-libIntegrationTests
 
+prettifyAbbrevations() {
+    sed 's/NS/Ns/g' | sed 's/URL/Url/g' | sed 's/HTTP/Http/g' | sed 's/JSON/Json/g'
+}
+
 for FILE in `ls *Test.m`
 do
     echo
+    FILE=`echo $FILE | prettifyAbbrevations`
     echo ">>>${FILE}:" | sed 's/\.m//g' | sed 's/Test:/:/g' | sed 's/\([A-Z]\)/_\1/g' | sed "s/_/ /g"
-    grep -h "(void)test" "$FILE" | cut -d '_' -f 2- | sed 's/\([A-Z]\)/_\1/g' | sed "s/_/ /g" | tr '[A-Z]' '[a-z]' | awk '''{ printf ("%s%s\n", toupper (substr ($0, 1, 1)), substr ($0, 2)); }''' | sed 's/.*/- &/g'
+    grep -h "(void)test" "$FILE" | prettifyAbbrevations | cut -d '_' -f 2- | sed 's/\([A-Z]\)/_\1/g' | sed "s/_/ /g" | tr '[A-Z]' '[a-z]' | awk '''{ printf ("%s%s\n", toupper (substr ($0, 1, 1)), substr ($0, 2)); }''' | sed 's/.*/- &/g'
 done
     
